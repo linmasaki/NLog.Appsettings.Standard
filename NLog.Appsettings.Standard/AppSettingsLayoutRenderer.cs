eae8cd4fc98d9d40f84b1feaf4c91624262af257
CoCo <linmasaki@users.noreply.github.com>
2019-01-10 16:37:20 +0800

    Adjust code format...

--- NLog.Appsettings.Standard/AppSettingsLayoutRenderer.cs
+++ NLog.Appsettings.Standard/AppSettingsLayoutRenderer.cs
@@ -12,72 +12,70 @@ namespace NLog.Appsettings.Standard
     {
         private static IConfigurationRoot _configurationRoot;
 
-		internal IConfigurationRoot DefaultAppSettings
-		{
-			get => _configurationRoot;
-		    set => _configurationRoot = value;
-		}
+	internal IConfigurationRoot DefaultAppSettings
+	{
+	    get => _configurationRoot;
+	    set => _configurationRoot = value;
+	}
 
-		/// <summary>
+	/// <summary>
         /// Global configuration. Used if it has set
         /// </summary>
         public static IConfiguration AppSettings { private get; set; }
 
         ///<summary>
-		/// The AppSetting name.
-		///</summary>
-		[RequiredParameter]
-		[DefaultParameter]
-		public string Name { get; set; }
+	/// The AppSetting name.
+	///</summary>
+	[RequiredParameter]
+	[DefaultParameter]
+	public string Name { get; set; }
 
-		///<summary>
-		/// The default value to render if the AppSetting value is null.
-		///</summary>
-		public string Default { get; set; }		
+	///<summary>
+	/// The default value to render if the AppSetting value is null.
+	///</summary>
+	public string Default { get; set; }		
 
         public AppSettingsLayoutRenderer() {
 			
             if(AppSettings == null && DefaultAppSettings == null)
-			{
-				DefaultAppSettings = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
-                			.AddJsonFile("appsettings.json")
-                			.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true).Build();
-			}
+	    {
+	        DefaultAppSettings = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+        		.AddJsonFile("appsettings.json")
+        		.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true).Build();
+	    }
         }
 
         /// <summary>
-		/// Renders the specified application setting or default value and appends it to the specified <see cref="StringBuilder" />.
-		/// </summary>
-		/// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
-		/// <param name="logEvent">Logging event.</param>
+	/// Renders the specified application setting or default value and appends it to the specified <see cref="StringBuilder" />.
+	/// </summary>
+	/// <param name="builder">The <see cref="StringBuilder"/> to append the rendered data to.</param>
+	/// <param name="logEvent">Logging event.</param>
         protected override void Append(StringBuilder builder, LogEventInfo logEvent)
         {
-            if (Name == null)
-				return;            
+            if (Name == null) return;            
 
             string value = AppSettingValue;
-			if (value == null && Default != null)
-				value = Default;
+	    if (value == null && Default != null)
+		value = Default;
 
             if (string.IsNullOrEmpty(value) == false)
-				builder.Append(value);
+		builder.Append(value);
         }
 
         private bool _cachedAppSettingValue = false;
-		private string _appSettingValue = null;		
-		private string AppSettingValue
-		{
-			get
-			{
+	private string _appSettingValue = null;		
+	private string AppSettingValue
+	{
+	    get
+	    {
                 Name = Name.Replace('.',':');
-				if (_cachedAppSettingValue == false)
-				{
-					_appSettingValue = AppSettings == null ? DefaultAppSettings[Name] : AppSettings[Name];
-					_cachedAppSettingValue = true;
-				}
-				return _appSettingValue;
-			}
+		if (_cachedAppSettingValue == false)
+		{
+		    _appSettingValue = AppSettings == null ? DefaultAppSettings[Name] : AppSettings[Name];
+		    _cachedAppSettingValue = true;
 		}
-
+	        return _appSettingValue;
+	    }
+	}
     }
 }
